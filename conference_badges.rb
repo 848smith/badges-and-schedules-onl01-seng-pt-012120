2d7162737ec492fdf53aeeadc42064540638dc36
Andrew Smith <848smith@gmail.com>
2020-01-31 05:10:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,6 +10,6 @@ end
 
 def assign_rooms(attendees)
   attendees.map do |attendee, index|
-  "Hello, #{attendee}! You'll be assigned to room #{index+ 1}!"
+  "Hello, #{attendee}! You'll be assigned to room #{index + 1}!"
   end
 end
