6912bea35de243cd04f849bbfcfe45acd0aa6f71
Andrew Smith <848smith@gmail.com>
2020-01-31 05:08:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,9 +9,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  counter = 0
-  attendees.map do |attendee|
-  "Hello, #{attendee}! You'll be assigned to room #{counter}!"
-  counter += 1
+  attendees.map do |attendee,index|
+  "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
   end
 end
